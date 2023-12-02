4c4158dfb448a89ccdaf973eb3c06a5cf3fb9e71
Ahmed Mabrouk Ahmed <ahmdmbrwk80@gmail.com>
2023-12-02 17:45:33 +0200

    Add project files.

---
+++ GameZone/Controllers/GamesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameZone.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ GameZone/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿using GameZone.Models;
+
+namespace GameZone.Models
+{
+    public class Category : BaseEntity
+    {
+        public ICollection<Game> Games { get; set; } = new List<Game>();
+    }
+}
